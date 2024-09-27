50cbf5dec4dece8918ad0758fbd30f0351457129
maddie-li <madelineli.van@gmail.com>
2024-09-27 15:17:53 -0700

    A1DiceGame 1
    Renaming files
    Making turns count more efficient by going by number of dice

---
+++ GD12_1133_Lab4_Maddie_Li/GD12_1133_Lab4_Maddie_Li/AssignmentText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD12_1133_Lab4_Maddie_Li
+{
+    internal class AssignmentText
+    {
+        internal void Intro()
+        {
+            // instantiating
+            GameManager manager = new GameManager();
+
+            Console.WriteLine("Welcome to Maddie Li's Lab 4 (2024-09-27)\n");
+            manager.GameStart();
+        }
+    }
+}
